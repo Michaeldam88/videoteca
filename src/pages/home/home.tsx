178ba727601c74bd554e341f6f58c79388063136
Michaeldam88 <michaeldam88@gmail.com>
2023-01-24 17:09:53 +0100

    added a part of movie card css

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,21 +1,24 @@
+import { Search } from '../../components/search/search';
+
 export default function Home() {
     return (
         <main className="home">
-            <div className="home__search-container">
-                <div className="home__search-box">
-                    <span className="material-symbols-outlined">
-                        saved_search
-                    </span>
-                    <input
-                        className="home__search"
-                        type="text"
-                        placeholder="Buscar"
-                    />
-                </div>
-                <span className="home__filter material-symbols-outlined">
-                    filter_alt
-                </span>
-            </div>
+            <Search></Search>
+            <h1 className="home__title">Peliculas Populares</h1>
+            <ul className="movies-list">
+                <li>
+                    <img src="" alt="pelicula1" />
+                    <div>
+                        {' '}
+                        <span className="material-symbols-outlined">star</span>
+                    </div>
+                    <h4>terror</h4>
+                    <div>
+                        <h2>Film Title</h2>
+                        <h3>2012</h3>
+                    </div>
+                </li>
+            </ul>
         </main>
     );
 }
