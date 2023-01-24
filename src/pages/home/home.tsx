66f716dbb0d3416be1814319b428cbf4bd9ed1c6
Michaeldam88 <michaeldam88@gmail.com>
2023-01-24 14:10:11 +0100

    added the search css

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -2,14 +2,20 @@ export default function Home() {
     return (
         <main className="home">
             <div className="home__search-container">
-                <span className="material-symbols-outlined">saved_search</span>
-                <input
-                    className="home__search"
-                    type="text"
-                    placeholder="Buscar"
-                />
+                <div className="home__search-box">
+                    <span className="material-symbols-outlined">
+                        saved_search
+                    </span>
+                    <input
+                        className="home__search"
+                        type="text"
+                        placeholder="Buscar"
+                    />
+                </div>
+                <span className="home__filter material-symbols-outlined">
+                    filter_alt
+                </span>
             </div>
-            <span className="material-symbols-outlined">filter_alt</span>
         </main>
     );
 }
