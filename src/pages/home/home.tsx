a6562e9ffd6f2774cf872ebcc2c377e57c641a50
Michaeldam88 <michaeldam88@gmail.com>
2023-01-25 19:29:47 +0100

    added the pagination css

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,6 +1,27 @@
+import TablePagination from '@mui/material/TablePagination';
+import React from 'react';
 import { Search } from '../../components/search/search';
 
 export default function Home() {
+
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+    const handleChangePage = (
+        event: React.MouseEvent<HTMLButtonElement> | null,
+        newPage: number
+    ) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (
+        event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+    ) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
+
+
     return (
         <main className="home">
             <Search></Search>
@@ -101,6 +122,16 @@ export default function Home() {
                     </div>
                 </li>
             </ul>
+            
+            
+            <TablePagination
+                component="div"
+                count={100}
+                page={page}
+                onPageChange={handleChangePage}
+                rowsPerPage={rowsPerPage}
+                onRowsPerPageChange={handleChangeRowsPerPage}
+            />
         </main>
     );
 }
