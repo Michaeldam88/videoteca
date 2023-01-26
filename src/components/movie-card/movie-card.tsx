f1cc2d2c1d9d783fffa6962a02399aee08fc8e8e
Michaeldam88 <michaeldam88@gmail.com>
2023-01-26 14:36:31 +0100

    componentization of html

---
+++ src/components/movie-card/movie-card.tsx
@@ -0,0 +1,19 @@
+export function MovieCard () {
+    return (
+        <li className="movie-card">
+            <img
+                className="movie-card__img"
+                src="https://image.tmdb.org/t/p/w500/ngvcBGzUbPsUGaSSLOaVNG2lfLW.jpg"
+                alt="pelicula1"
+            />
+            <div className="movie-card__top">
+                <span className="material-symbols-outlined">star</span>
+            </div>
+            <h4 className="movie-card__tag">terror</h4>
+            <div className="movie-card__bottom">
+                <h2 className="movie-card__title">Film Title</h2>
+                <h3 className="movie-card__year">2012</h3>
+            </div>
+        </li>
+    );
+} 
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -24,102 +24,7 @@ export default function Home() {
         <main className="home">
             <Search></Search>
             <h1 className="home__title">Peliculas Populares</h1>
-            <ul className="movies-list">
-                <li className="movie-card">
-                    <img
-                        className="movie-card__img"
-                        src="https://image.tmdb.org/t/p/w500/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg"
-                        alt="pelicula1"
-                    />
-                    <div className="movie-card__top">
-                        <span className="material-symbols-outlined">star</span>
-                    </div>
-                    <h4 className="movie-card__tag">terror</h4>
-                    <div className="movie-card__bottom">
-                        <h2 className="movie-card__title">Film Title</h2>
-                        <h3 className="movie-card__year">2012</h3>
-                    </div>
-                </li>
-
-                <li className="movie-card">
-                    <img
-                        className="movie-card__img"
-                        src="https://image.tmdb.org/t/p/w500/ngvcBGzUbPsUGaSSLOaVNG2lfLW.jpg"
-                        alt="pelicula1"
-                    />
-                    <div className="movie-card__top">
-                        <span className="material-symbols-outlined">star</span>
-                    </div>
-                    <h4 className="movie-card__tag">terror</h4>
-                    <div className="movie-card__bottom">
-                        <h2 className="movie-card__title">Film Title</h2>
-                        <h3 className="movie-card__year">2012</h3>
-                    </div>
-                </li>
-                <li className="movie-card">
-                    <img
-                        className="movie-card__img"
-                        src="https://image.tmdb.org/t/p/w500/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg"
-                        alt="pelicula1"
-                    />
-                    <div className="movie-card__top">
-                        <span className="material-symbols-outlined">star</span>
-                    </div>
-                    <h4 className="movie-card__tag">terror</h4>
-                    <div className="movie-card__bottom">
-                        <h2 className="movie-card__title">Film Title</h2>
-                        <h3 className="movie-card__year">2012</h3>
-                    </div>
-                </li>
-
-                <li className="movie-card">
-                    <img
-                        className="movie-card__img"
-                        src="https://image.tmdb.org/t/p/w500/ngvcBGzUbPsUGaSSLOaVNG2lfLW.jpg"
-                        alt="pelicula1"
-                    />
-                    <div className="movie-card__top">
-                        <span className="material-symbols-outlined">star</span>
-                    </div>
-                    <h4 className="movie-card__tag">terror</h4>
-                    <div className="movie-card__bottom">
-                        <h2 className="movie-card__title">Film Title</h2>
-                        <h3 className="movie-card__year">2012</h3>
-                    </div>
-                </li>
-
-                <li className="movie-card">
-                    <img
-                        className="movie-card__img"
-                        src="https://image.tmdb.org/t/p/w500/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg"
-                        alt="pelicula1"
-                    />
-                    <div className="movie-card__top">
-                        <span className="material-symbols-outlined">star</span>
-                    </div>
-                    <h4 className="movie-card__tag">terror</h4>
-                    <div className="movie-card__bottom">
-                        <h2 className="movie-card__title">Film Title</h2>
-                        <h3 className="movie-card__year">2012</h3>
-                    </div>
-                </li>
-
-                <li className="movie-card">
-                    <img
-                        className="movie-card__img"
-                        src="https://image.tmdb.org/t/p/w500/ngvcBGzUbPsUGaSSLOaVNG2lfLW.jpg"
-                        alt="pelicula1"
-                    />
-                    <div className="movie-card__top">
-                        <span className="material-symbols-outlined">star</span>
-                    </div>
-                    <h4 className="movie-card__tag">terror</h4>
-                    <div className="movie-card__bottom">
-                        <h2 className="movie-card__title">Film Title</h2>
-                        <h3 className="movie-card__year">2012</h3>
-                    </div>
-                </li>
-            </ul>
+            <ul className="movies-list"></ul>
 
             <TablePagination
                 component="div"
@@ -129,10 +34,7 @@ export default function Home() {
                 rowsPerPage={rowsPerPage}
                 onRowsPerPageChange={handleChangeRowsPerPage}
             />
-            <div className="modal">
-                
-            </div>
-            <article></article>
+            {/* <div className="modal"></div> */}
         </main>
     );
 }
